5dea65b25e8821f6714aaefede40c50a2a046be3
Ido Shipony <idoshi123@gmail.com>
2025-12-26 15:12:05 +0200

    complete assigment

--- Binomial.java
+++ Binomial.java
@@ -23,8 +23,8 @@ public class Binomial {
 	 }
 	
 	// Computes the Binomial function, efficiently
-	public static int binomial(int n, int k) {
-		int[][] memo = new int[n + 1][k + 1];
+	public static long binomial(int n, int k) {
+		long[][] memo = new long[n + 1][k + 1];
 
    
          for (int i = 0; i <= n; i++) {
@@ -33,12 +33,12 @@ public class Binomial {
         }
     }
 
-    // קריאה לפונקציה שעושה את העבודה הקשה
+   
     return binomial(n, k, memo);
 		
 	}
 
-	private static int binomial(int n, int k, int[][] memo) {
+	private static long binomial(int n, int k, long[][] memo) {
 		if (memo[n][k] != -1) {
 			return memo[n][k];
 		}
